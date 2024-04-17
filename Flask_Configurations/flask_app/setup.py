791b3bea3e6ce69d588c0056d95a87fdbb1ddb59
AbMohiz <abmohiz22@gmail.com>
2024-04-17 10:13:08 +0500

    Delete the Setup for making app run as a package.

--- Flask_Configurations/flask_app/setup.py
+++ Flask_Configurations/flask_app/setup.py
@@ -2,26 +2,3 @@
 # -*- coding: UTF-8 -*-
 import os
 from setuptools import setup
-setup(
-    name = 'my_app',
-    version='1.0',
-    license='GNU General Public License v3',
-    author='Ab mohiz',
-    author_email='abmohiz@gmail.com',
-    description='Hello world application for Flask',
-    packages=['my_app'],
-    platforms='any',
-    install_requires=[
-        'Flask',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries ::Python Modules'
-],
-)
